34b73ebf846ea68a5d377ba474698bd272671a27
Aung Kyaw Nyunt <aungkyawnyunt2004@gmail.com>
2024-08-15 01:14:49 +0700

    [modify] : remove testfun on servicecheck route

--- src/app/routes/api/serviccheck.route.ts
+++ src/app/routes/api/serviccheck.route.ts
@@ -8,9 +8,6 @@ import { REQUEST_METHODS } from '@/config';
 const { GET, POST } = REQUEST_METHODS;
 const routers = new Routers();
 
-const testFun = (req: Request, res: Response, next?: NextFunction) => {
-    res.status(200).json({ 'msg': 'tes testFunt' })
-}
 export default routers
     .addRoute([GET], '/health-check', ServiceCheckController.healthCheck)
     .addRoute([GET, POST], '/is-maintenance', ServiceCheckController.isMaintenance);
